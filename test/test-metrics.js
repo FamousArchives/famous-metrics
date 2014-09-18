7615ca578a3996d0dc9374c446ae7e9f3eccb444
Myles Borins <myles@famo.us>
2014-09-17 18:43:33 -0700

    Add test setup / teardown

--- test/test-metrics.js
+++ test/test-metrics.js
@@ -1,7 +1,32 @@
+'use strict';
+var fs = require('fs');
+var path = require('path');
+
 var test = require('tape');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
 var metrics = require('../index.js');
 
+var fakeHome = path.join(String(process.env.TMPDIR), 'famous-metrics-test');
+
+test('Did it setup correctly', function (t) {
+  t.plan(1);
+  mkdirp.sync(fakeHome);
+  fs.exists(fakeHome, function (exists) {
+    t.ok(exists, ['The folder', fakeHome, 'should exist after setup'].join(' '));
+  });
+});
+
 test('Does it load?',  function (t) {
-  t.ok(metrics, "It loads!!!");
-  t.end();
+  t.plan(1);
+  t.ok(metrics, 'It loads!!!');
+});
+
+test('Did it tear down correctly', function (t) {
+  t.plan(1);
+  rimraf.sync(fakeHome);
+  fs.exists(fakeHome, function (exists) {
+    t.notOk(exists, ['The folder', fakeHome, 'should not exist after teardown'].join(' '));
+  });
 });
